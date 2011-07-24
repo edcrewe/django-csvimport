5951928b090b45497a413de99dba090c700bdade
Ed Crewe <edmundcrewe@gmail.com>
2011-07-24 18:26:50 +0100

    use rst extension for egg setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ version = '0.1'
 setup(name='django-csvimport',
       version=version,
       description="Import CSV files to django models",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
