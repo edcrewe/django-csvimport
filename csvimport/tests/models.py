23174b5b1d26816fe9f255e2426d2917618dfb2a
Ed Crewe <edmundcrewe@gmail.com>
2011-07-24 19:57:41 +0100

    add fixtures and test models

---
+++ csvimport/tests/models.py
@@ -0,0 +1,50 @@
+# Test case models for cvsimport
+from django.db import models
+
+class Country(models.Model):
+    """
+    ISO country (location) codes.
+    and lat long for Geopoint Mapping
+    """
+    code = models.CharField(max_length=4, primary_key=True)
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    alias = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return u"%s (%s)" % (self.name, self.code)
+
+
+class UnitOfMeasure(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Item(models.Model):
+    TYPE = models.PositiveIntegerField(default=0)
+    code_share = models.CharField(
+        max_length=32,
+        help_text = "Cross-organization item code")
+    code_org = models.CharField(
+        max_length=32,
+        help_text="Organization-specfific item code")
+    description = models.TextField(null=True)
+    quantity = models.PositiveIntegerField(default=1)
+    uom = models.ForeignKey(UnitOfMeasure,
+                            help_text = 'Unit of Measure')
+    organisation = models.ForeignKey(Organisation)
+    status = models.CharField(max_length = 10, null=True)
+    date = models.DateField(auto_now=True, null=True, validators=[])
+    country = models.ForeignKey(Country)
+
+
--- csvimport/tests/parse_tests.py
+++ csvimport/tests/parse_tests.py
@@ -1,10 +1,18 @@
-from django.test import TestCase
-#from utils import parse_excel_upload
 import os
 
+from django.test import TestCase
+from csvimport.management.commands.csvimport import Command
+
 class CommandParseTest(TestCase):
-    
-    test_upload_path = 'fixtures/test_upload.xls'
+    """ Run test of file parsing """
+    test_upload_path = 'fixtures/test.csv'
     
     def test_parse(self):
-      self.parse(self.test_upload_path)
+        cmd = Command()
+        test_fh = os.open(self.test_upload_path)
+        cmd.setup(mappings='', 
+                  modelname='csvimport.tests.Item', 
+                  uploaded=test_fh,
+                  defaults='')
+        errors = cmd.run(logid=obj.id)
+        print errors
