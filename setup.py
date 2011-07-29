71442076dc00ccd83b83950ad815a0d657dc0564
Ed Crewe <edmundcrewe@gmail.com>
2011-07-29 11:12:43 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='django-csvimport',
       version=version,
       description="Import CSV files to django models",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "TODO.rst").read() + "\n" +
+                       open(os.path.join("docs", "TODO.rst")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
