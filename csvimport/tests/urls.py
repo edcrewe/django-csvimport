7a9c1c7ecb439b28d08809f9bcab8806c3bd7c33
Ed Crewe <edmundcrewe@gmail.com>
2014-02-24 13:02:18 +0000

    make urls import compatible with django 1.6

--- csvimport/tests/urls.py
+++ csvimport/tests/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from csvimport.tests.views import index
