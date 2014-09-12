ff0e85ca02ca5f1b72a6c9cc4cb0461b21910bf0
edcrewe <ed.crewe@bristol.ac.uk>
2014-09-12 09:14:22 +0100

    add file that fixes limit 1970-2037

---
+++ csvimport/monkeypatch_tzinfo.py
@@ -0,0 +1,27 @@
+""" Monkeypatch so that dates outside of 1970-2037 can be imported for
+    the DB backends that do not handle this
+"""
+import time
+from django.utils.timezone import ReferenceLocalTimezone
+
+def _isdst(self, dt):
+    """ Monkeypatch from https://code.djangoproject.com/ticket/3418
+        since sqlite and other backends still have this issue in django 1.6
+    """
+    year = dt.year
+    if int(year) < 1970:
+        year = 1970
+    if int(year) > 2037:
+        year = 2037
+    tt = (year, dt.month, dt.day, dt.hour, dt.minute, dt.second, dt.weekday(), 0, -1)
+    try:
+        stamp = time.mktime(tt)
+        tt = time.localtime(stamp)
+        return tt.tm_isdst > 0
+    except OverflowError:
+        pass
+    raise Exception('Cannot process dates from %s' % year)
+
+ReferenceLocalTimezone._isdst = _isdst
+
+
