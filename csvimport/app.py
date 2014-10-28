58a9f66c24944b45dec69bcfb06f6567baa68958
edcrewe <edmundcrewe@gmail.com>
2014-10-28 12:46:51 +0000

    fix running tests when pypi installed

--- csvimport/app.py
+++ csvimport/app.py
@@ -2,14 +2,9 @@ import os
 from django.apps import AppConfig
 from django.conf import settings
 
-
 class CSVImportConf(AppConfig):
     name = 'csvimport'
     verbose_name = 'CSV importer'
     MODELS = []
     MEDIA_ROOT = settings.MEDIA_ROOT
-
-    @classmethod
-    @property
-    def path(cls):
-        return os.path.abspath(cls.__module__.__file__)
+    path = os.path.abspath(__file__)
--- csvimport/settings.py
+++ csvimport/settings.py
@@ -29,8 +29,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'csvimport',
-    'csvimport.tests'
+    'csvimport.app.CSVImportConf',
 )
 
 MIDDLEWARE_CLASSES = (
