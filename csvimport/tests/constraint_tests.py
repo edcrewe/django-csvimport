4f4653820ca540c51b963f9b134a4a572c327128
Ed Crewe <ed.crewe@oracle.com>
2017-05-22 09:58:00 +0100

    add in country test

---
+++ csvimport/tests/constraint_tests.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Use unicode source code to make test character string writing easier
+from csvimport.tests.testcase import CommandTestCase
+from csvimport.tests.models import Country
+import sys
+pyversion = sys.version_info[0]  # python 2 or 3
+
+
+class ConstraintTest(CommandTestCase):
+    """ Run test of file parsing """
+
+    def get_country(self, country_code):
+        try:
+            country = Country.objects.get(code__exact=country_code)
+        except ObjectDoesNotExist:
+            country = None
+            self.assertTrue(country, 'Failed to get row from imported test csv for countries')
+        return country
+
+    def test_empty_notnull(self, filename='bad_country.csv'):
+        """ Use custom command to upload a country file with missing long lat data"""
+        errs = ['Imported 3 rows to Country']
+        self.command(csvfile=filename, modelname='csvimport.Country', defaults='',
+                     expected_errs=errs)
+        self.assertEqual(Country.objects.count(), 3)
+        country = self.get_country('MS')
+        self.assertTrue(country.name, "Montserrat")
+        Country.objects.all().delete()
