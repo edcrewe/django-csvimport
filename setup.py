75809e119bc8e2b8dc3fc03017508d60d3fcc44f
Ed Crewe <ed.crewe@oracle.com>
2017-05-30 17:17:39 +0100

    force to bump version for comment in tests settings file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '2.6'
+version = '2.7'
 
 with open("README.rst", "r") as fp:
     csvimport_description = fp.read() + "\n"
