ce12337d6deace40a60ebdc28868c7cae4e80a2a
Ed Crewe <ed.crewe@forgerock.com>
2020-04-27 13:30:13 +0100

    fix rst format and rebuild new version since 2.14 failed to package __init__

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "2.14"
+version = "2.15"
 
 with open("README.rst", "r") as fp:
     csvimport_description = fp.read() + "\n"
