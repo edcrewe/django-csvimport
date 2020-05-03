82043953ea072c1d6b9bceeb98ba884f76b0b9f6
Ed Crewe <ed.crewe@forgerock.com>
2020-05-03 16:12:59 +0100

    create new 2.16 version to fix tests plus issues

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "2.15"
+version = "2.16"
 
 with open("README.rst", "r") as fp:
     csvimport_description = fp.read() + "\n"
